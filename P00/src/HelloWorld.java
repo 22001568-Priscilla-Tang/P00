3ddb0e8715b781b2235c5f68291a05d05521bab0
22001568 <22001568@10.174.121.112>
2023-05-29 11:46:00 +0800

    Added change B

---
+++ P00/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22001568, May 29, 2023 9:09:13 AM
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println ("My first java program");
+	}
+
+}
